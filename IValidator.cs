3ffb1dab25cac7959bbcb0268082c80fb472d7a7
Jack Gittoes <Jrgittoes@gmail.com>
2021-06-11 19:40:39 +0100

    Add project files.

---
+++ IValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SummitNeedsChecking
+{
+    public interface IValidator
+    {
+        bool ReferenceIsAtLeast8Characters(string reference);
+        bool ReferenceStartsWithLetter(string reference);
+        bool ReferenceOnlyContainsLettersAndNumbers(string reference);
+        bool ReferenceContainsChecksum(string reference);
+    }
+}
---
+++ Validator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SummitNeedsChecking
+{
+    public class Validator : IValidator
+    {
+        public bool ReferenceContainsChecksum(string reference)
+        {
+            var length = reference.Length;
+            return Char.IsDigit(reference[length - 2]) && Char.IsDigit(reference[length - 1]);
+        }
+
+        public bool ReferenceIsAtLeast8Characters(string reference)
+        {
+            return reference.Length >= 8;
+        }
+
+        public bool ReferenceOnlyContainsLettersAndNumbers(string reference)
+        {
+            var regex = new Regex(@"^[_A-z0-9]*((-|\s)*[_A-z0-9])*$");
+            return regex.IsMatch(reference);
+        }
+
+        public bool ReferenceStartsWithLetter(string reference)
+        {
+            return Char.IsLetter(reference[0]);
+        }
+    }
+}
